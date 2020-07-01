e9f40a8cdc800dd4c196f75db9082d7040c2d4b7
hkhajanchi <khajanh1@tcnj.edu>
2020-07-01 13:25:29 -0400

    added src files for MAC

---
+++ src/main/scala/lstm/mac_param.scala
@@ -0,0 +1,39 @@
+package mac 
+
+import chisel3._ 
+
+/*
+A Simple Multiply-Accumulate Unit (MAC) parameterized by operand bitwidths "a" and "b"
+
+Returns a signed integer of bitwidth "a"+"b"
+
+Class Parameters: 
+    @a : bitwidth for west port
+    @b : bitwidth for north port 
+    @c:  bitwidth for weight
+*/
+
+class MacParam(val a:Int, val b:Int, val c:Int) extends Module {
+    
+    val io = IO( new Bundle{
+
+    val west  = Input(SInt(a.W))
+    val north = Input(SInt(b.W))
+    
+    val weight = Input(SInt(c.W))
+
+    val south = Output(SInt()) 
+    val east = Output(SInt()) 
+    
+    })
+
+
+    // Latch the multiplication - available after clk 1 
+    val x = RegNext(io.west * io.weight)
+    val east_reg = RegNext(io.west)
+
+    //available after clk 2 
+    io.south := x + io.north //assuming 0 clk latency for add 
+    io.east  := east_reg 
+}
+
---
+++ src/test/scala/lstm/mac_tb.scala
@@ -0,0 +1,50 @@
+package mac 
+
+import chisel3._ 
+import chisel3.iotesters
+import chisel3.iotesters.{PeekPokeTester, Driver, ChiselFlatSpec}
+
+import chisel3.experimental.DataMirror 
+
+class MacParamUnitTester (c:MacParam) extends PeekPokeTester(c) {
+
+    /* Create random signed integers using Scala's nextInt() generator 
+    range: -(2^(bitwitdth-1)) to 2^(bitwidth) where a,b,c are the respective operand bitwidths */
+
+    def createRandInt(width:Int) : Int = rnd.nextInt(1<<width) - (1<<(width-1))
+    
+    for (i <- 0 until 100 ) {
+
+ 
+        val testn   = createRandInt(c.b)
+        val testw   = createRandInt(c.a)
+        val testwgt = createRandInt(c.c)  
+
+        /* Expected MAC values */ 
+        val out_e = testw 
+        val out_s = (testw * testwgt)  + testn 
+
+        poke(c.io.west, testw)
+        poke(c.io.north, testn)
+        poke(c.io.weight, testwgt)
+        
+        step(1)
+
+        expect(c.io.east, out_e)
+        expect(c.io.south, out_s)
+
+    }
+
+}
+
+class MacParamTester extends ChiselFlatSpec {
+
+    "Mac Unit" should "multiply & accumulate input ports" in { 
+    // def createrandMac : T = {
+    //     val x = new MacParam(16,16,16)
+    // }
+    iotesters.Driver.execute(Array[String]("--is-verbose", "--generate-vcd-output","on","--target-dir","test/vcd","--top-name","vcd"),() => new MacParam(16,16,16))
+    {
+        c => new MacParamUnitTester(c)
+    } should be(true) }
+}
